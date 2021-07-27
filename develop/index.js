13e3fe991e314e60615bb3567b94451a1cc46e7d
Matias Minchaca <matiasminchaca1@gmail.com>
2021-07-27 16:53:52 -0300

    loginFormV1.0

---
+++ develop/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+app.get('/loginConfirmation', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/loginConfirmation.html'))
+})
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get('/shoppingCart', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/shoppingCart.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`)
+})
